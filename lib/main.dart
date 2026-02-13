ac9057b2742927bdd0f777703c4d3297f2c55c14
KicaiBadai <adityamaula80@gmail.com>
2026-02-13 11:40:04 +0700

    3

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'fruit catcher game ', home: const GameScreen());
-  }
-}
+  const MyApp({super.key});
 
-class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Game Akan Ditampilkan Di sini")));
+    return MaterialApp(title: 'Fruit Catcher Game', home: const GameScreen());
   }
 }
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
+
   @override
   State<GameScreen> createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
   @override
-  final ValueNotifier<int> counter = ValueNotifier(1);
-
   void initState() {
     super.initState();
   }
@@ -46,16 +40,8 @@ class _GameScreenState extends State<GameScreen> {
                 color: Colors.black54,
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: const Text(
-                'Score: 1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ), //Container
-          ), // Positioned
+            ),
+          ),
         ],
       ),
     );
